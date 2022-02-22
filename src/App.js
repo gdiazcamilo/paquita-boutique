ab4b646b4efbdbebee079d3051664302a669c761
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-02-22 14:24:36 -0400

    add navigation to hats page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from "react";
-import { Routes, Route, Link, useParams } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
 import { HomePage } from "./pages/homepage.component";
 
-const HatsPage = (props) => {
+const HatsPage = () => {
   return (
     <div>
       <h1>Hats page</h1>
-      <p>
-        <Link to='12'>Hat 12</Link>
-      </p>
-      <p>
-        <Link to='182'>Hat 12</Link>
-      </p>
-    </div>
-  );
-};
-
-const HatsDetail = (props) => {
-  let params = useParams();
-  console.log(params);
-  return (
-    <div>
-      <h1>{props.p1}</h1>
-      <div>
-        <h1>{`Hats detail ${params.hatId}`}</h1>
-      </div>
     </div>
   );
 };
@@ -38,7 +19,6 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/hats' element={<HatsPage />} />
-        <Route path='/hats/:hatId' element={<HatsDetail p1={23} />} />
       </Routes>
     </div>
   );
