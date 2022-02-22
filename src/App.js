1d9b9d94bb467303530fff350de93736c4a76a46
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-02-22 13:53:58 -0400

    playing with routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useParams } from "react-router-dom";
 
 import "./App.css";
 
 import { HomePage } from "./pages/homepage.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>Hats page</h1>
+      <p>
+        <Link to='12'>Hat 12</Link>
+      </p>
+      <p>
+        <Link to='182'>Hat 12</Link>
+      </p>
+    </div>
+  );
+};
+
+const HatsDetail = (props) => {
+  let params = useParams();
+  console.log(params);
+  return (
+    <div>
+      <h1>{props.p1}</h1>
+      <div>
+        <h1>{`Hats detail ${params.hatId}`}</h1>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -17,6 +38,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/hats' element={<HatsPage />} />
+        <Route path='/hats/:hatId' element={<HatsDetail p1={23} />} />
       </Routes>
     </div>
   );
