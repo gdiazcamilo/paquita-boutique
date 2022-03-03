7b150ab2aba9e5a5c2fdcfb8daa9a159801e8c6d
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-03-03 14:51:13 -0400

    implement sign with email and password

--- src/pages/sign-up-and-sign-in/sign-up-and-sign-in.component.jsx
+++ src/pages/sign-up-and-sign-in/sign-up-and-sign-in.component.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import "./sign-up-and-sign-in.styles.scss";
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 export const SignUpAndSignIn = () => (
   <div className='sign-up-and-sign-in'>
     <SignIn />
+    <SignUp />
   </div>
 );
