f8ac2d579f230ed829f870fb1fd7013e4ae88ae0
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-03-14 14:54:22 -0400

    show items in cart dropdown preview

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
 
+import CartDropdownItem from "../cart-dropdown-item/cart-dropdown-item.component";
 import { CustomButton } from "../custom-button/custom-button.component";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
-    <div className='cart-item-list'></div>
+    <div className='cart-item-list'>
+      {cartItems.map((item) => (
+        <CartDropdownItem key={item.id} item={item} />
+      ))}
+    </div>
     <CustomButton>SHOW ALL</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+export default connect(
+  (state) => ({ cartItems: state.cart.cartItems }),
+  null
+)(CartDropdown);
