e1505b1a583f52f76adec2696eed3656fb461a7b
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-03-21 14:30:01 -0400

    refactor: create selector for selecting collections hashmap as array using Object.values to display previews

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectCatalogCollections } from "../../redux/catalog/catalog.selectors";
+import { selectCatalogCollectionList } from "../../redux/catalog/catalog.selectors";
 
 import { MenuItem } from "../menu-item/menu-item.component";
 
@@ -10,13 +10,13 @@ import "./directory.styles.scss";
 
 const Directory = ({ collections }) => (
   <div className='directory-menu'>
-    {Object.values(collections).map(({ id, ...otherSectionProps }) => (
+    {collections.map(({ id, ...otherSectionProps }) => (
       <MenuItem key={id} {...otherSectionProps} />
     ))}
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCatalogCollections,
+  collections: selectCatalogCollectionList,
 });
 export default connect(mapStateToProps)(Directory);
