17e64ddb96bf42a46f73a0193416913db4d5fdc5
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-03-25 14:36:05 -0400

    migrating app from sass to styled components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,19 +1,6 @@
 import React from "react";
+import { AppButton } from "./custom-button.styles";
 
-import "./custom-button.styles.scss";
-
-export const CustomButton = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => (
-  <button
-    className={`custom-button ${inverted ? "inverted" : ""} ${
-      isGoogleSignIn ? "google-sign-in" : ""
-    }`}
-    {...otherProps}
-  >
-    {children}
-  </button>
+export const CustomButton = ({ children, ...otherProps }) => (
+  <AppButton {...otherProps}>{children}</AppButton>
 );
