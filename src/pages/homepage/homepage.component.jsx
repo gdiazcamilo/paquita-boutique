e66d6ac0be67aec52a686c1a34e2bb3bfbfbf6f6
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-04-01 13:58:22 -0400

    add loading screen

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,8 +3,10 @@ import React from "react";
 import { HomePageContainer } from "./homepage.styles";
 import Directory from "../../components/directory/directory.component";
 
-export const HomePage = () => (
+const HomePage = () => (
   <HomePageContainer>
     <Directory />
   </HomePageContainer>
 );
+
+export default HomePage;
