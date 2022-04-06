35bb0436d29e1ba70b0bbe81d7728612a6dcfa9e
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-04-06 14:42:04 -0400

    move loading logic to redux

--- src/redux/catalog/catalog.actions.js
+++ src/redux/catalog/catalog.actions.js
@@ -1,6 +1,34 @@
 import CartTypes from "./catalog.types";
 
-export const loadCollections = (collections) => ({
-  type: CartTypes.LOAD_COLLECTIONS,
+import { collection, getDocs } from "firebase/firestore";
+import {
+  firestore,
+  convertCollectionsSnapshotToObject,
+} from "../../firebase/firebase.utils";
+
+const loadCollectionsStart = () => ({
+  type: CartTypes.LOAD_COLLECTIONS_START,
+});
+const loadCollectionsSuccess = (collections) => ({
+  type: CartTypes.LOAD_COLLECTIONS_SUCCESS,
   payload: collections,
 });
+const loadCollectionsFail = (errorMessage) => ({
+  type: CartTypes.LOAD_COLLECTIONS_FAIL,
+  payload: errorMessage,
+});
+
+export const loadCollections = () => {
+  return (dispatch) => {
+    dispatch(loadCollectionsStart());
+
+    const collectionRef = collection(firestore, "collections");
+
+    getDocs(collectionRef)
+      .then((snapshot) => {
+        const collections = convertCollectionsSnapshotToObject(snapshot);
+        dispatch(loadCollectionsSuccess(collections));
+      })
+      .catch((error) => dispatch(loadCollectionsFail(error.message)));
+  };
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,13 +4,15 @@ import { persistStore } from "redux-persist";
 import persistedReducer from "./rootReducer";
 
 import { logger } from "redux-logger";
+import thunk from "redux-thunk";
 
-const middlewares = [];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
+console.log(middlewares);
 const store = createStore(persistedReducer, applyMiddleware(...middlewares));
 const persistor = persistStore(store);
 
