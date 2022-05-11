4e31764fce4b9e24d9210bc9a7e09961c86fb6c5
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-05-11 14:29:29 -0400

    separate client and backend for managing stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ amount }) => {
   const amountInCents = amount * 100;
@@ -7,7 +8,24 @@ const StripeCheckoutButton = ({ amount }) => {
     "pk_test_51KgBXxJkSf1EZaDlTc00kl5f9dTNFYUBYyGDifEjrZeuRumOTcwGzigq3JKIe0fSAtnZDuhuik63CiYODqlCAhAx00BKwmTn7z";
 
   const onCheckout = (token) => {
-    console.log(token);
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: amountInCents,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please sure you use the provided credit card"
+        );
+      });
   };
 
   return (
