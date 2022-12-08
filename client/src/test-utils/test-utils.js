8fb94291cdcff02ed2d84ebc9491e874c38922c1
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-12-08 11:22:54 -0400

    add utils to render with MemoryRouter

--- client/src/test-utils/test-utils.js
+++ client/src/test-utils/test-utils.js
@@ -2,10 +2,22 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { render } from "@testing-library/react";
 import rootReducer from "../redux/rootReducer";
+import { MemoryRouter } from "react-router-dom";
 
 const setupStore = (preloadedState) => {
   return configureStore({ reducer: rootReducer, preloadedState });
 };
+
+function withProviders(
+  ui,
+  { preloadedState = {}, store = setupStore(preloadedState) } = {}
+) {
+  return <Provider store={store}>{ui}</Provider>;
+}
+
+function withMemoryRouter(ui, options = {}) {
+  return <MemoryRouter {...options}>{ui}</MemoryRouter>;
+}
 
 function renderWithProviders(
   ui,
@@ -16,11 +28,37 @@ function renderWithProviders(
   } = {}
 ) {
   function Wrapper({ children }) {
-    return <Provider store={store}>{children}</Provider>;
+    return withProviders(children, preloadedState, store);
   }
 
   return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
 }
 
-export { renderWithProviders };
+function renderWithMemoryRouter(ui, options = {}, ...renderOptions) {
+  return render(withMemoryRouter(ui, options), { ...renderOptions });
+}
+
+function renderWithRouterAndProviders(
+  ui,
+  {
+    preloadedState = {},
+    store = setupStore(preloadedState),
+    routerOptions = {},
+  } = {},
+  ...renderOptions
+) {
+  return render(
+    withProviders(withMemoryRouter(ui, routerOptions), {
+      preloadedState,
+      store,
+    }),
+    { ...renderOptions }
+  );
+}
+
+export {
+  renderWithProviders,
+  renderWithMemoryRouter,
+  renderWithRouterAndProviders,
+};
 export * from "@testing-library/react";
